16d5e90c498bc244c104295e7ce51e1fa03af171
Florian Heinz <fh@vautron.de>
2021-12-02 08:33:56 +0100

    Fixed includefile random.h

--- main.c
+++ main.c
@@ -2,11 +2,11 @@
 #include "include/gobjects.h"
 #include "include/gevents.h"
 #include "include/simpio.h"
+#include "include/random.h"
 
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include <random.h>
 #include <time.h>
 
 int main (int argc, char **argv) {
